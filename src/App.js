654a46402be449cc91db83c31f30df16476b9fc1
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-09-06 16:42:43 +0900

    페이지 만들기

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Artist from './pages/Artist'
+import Musicvideo from './pages/Musicvideo'
+import Playlist from './pages/Playlist'
+import Channel from './pages/Channel'
+import Video from './pages/Video'
+import Search from './pages/Search'
+import Not from './pages/Not'
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />}/>
+				<Route path='/artist' element={<Artist />}/>
+				<Route path='/musicvideo' element={<Musicvideo/>}/>
+				<Route path='/playlist' element={<Playlist />}/>
+				<Route path='/channel/:channelId' element={<Channel />} />
+                <Route path='/video/:videoId' element={<Video />} />
+                <Route path='/search/:searchId' element={<Search />} />
+                <Route path='*' element={<Not />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App
+export default App;
