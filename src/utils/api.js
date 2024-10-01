6251f95716eaae9a29c7c9139941828b88a91cbf
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-10-01 17:23:52 +0900

    프록시추가

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
@@ -12,9 +11,9 @@ const options = {
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
     },
 };
-const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+
 export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${PROXY}/${BASE_URL}/${url}`, options);
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
     console.log(data)
     return data;
 };
